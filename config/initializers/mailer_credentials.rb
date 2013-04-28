38b9e4c461d29db890e32374ef1f53946af3da0d
Jaemar <jaemar.ramos@gmail.com>
2013-04-28 11:56:21 +0800

    set all config to larks

---
+++ config/initializers/mailer_credentials.rb
@@ -0,0 +1,8 @@
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  user_name: 'akosipc@gmail.com',
+  password: 'r3@Lcs2!',
+  authentication: 'plain',
+  enable_starttls_auto: true
+}
